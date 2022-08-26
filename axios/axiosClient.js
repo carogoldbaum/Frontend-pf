e6c43fbb4fb3ae731e53690a3f7bd1868baa7cbe
Android Studio <devandroid@ort>
2022-08-26 12:04:03 -0300

    iniciar sesion

--- axios/axiosClient.js
+++ axios/axiosClient.js
@@ -1,43 +1,74 @@
 import axios from "axios";
-import {TokenService} from '../services/TokenServices.js';
-
-TokenService.getToken();
 
 const axiosClient = axios.create ({
-    basedURL:"http://localhost:4000",
+    baseURL:"http://localhost:5000",
 
 })
 
-export const createUsuario = async () => {
-    return axiosClient.post('/rubro', {
-        headers: {
-            Authorization: 'Bearer ' + TokenService.getToken()
-        }
-    }).then(response => {
-        if(response.status < 300) {
-            return response.data
-    } else {
-        console.log("La llamada salio bien")
-    }
-    }).catch(error => {
-        console.log(error);
-        return error;
-    });
+export const getRubro = async () => {
+    return axiosClient.get('/rubro', {
+    
+    }).then(function(res){
+        return res.data.results
+    })
+    .catch(function(){
+        throw "Error"
+    })
 }
 
-export const getRubro = async () => {
-    return axiosClient.post('/rubro', {
-        headers: {
-            Authorization: 'Bearer ' + TokenService.getToken()
-        }
-    }).then(response => {
-        if(response.status < 300) {
-            return response.data
-    } else {
-        console.log("La llamada salio bien")
-    }
-    }).catch(error => {
-        console.log(error);
-        return error;
-    });
+export const postRegistrarse = async () => {
+    return axiosClient.post('/registrarse', {
+  
+    }).then(function(res){
+        return res.data.results
+    })
+    .catch(function(){
+        throw "Error"
+    })
+}
+
+export const postDatosPersonales = async () => {
+    return axiosClient.post('/DatosPersonales', {
+      
+    }).then(function(res){
+        return res.data.results
+    })
+    .catch(function(){
+        throw "Error"
+    })
+}
+
+export const getIniciarSesion = async (informacion) => {
+    console.log(informacion)
+    return axiosClient.post('/usuario/IniciarSesion', {data:informacion} 
+   
+    ).then(function(res){
+        
+        return res.data
+    })
+    .catch(function(){
+        throw e
+    })
+}
+
+export const postRestablecer = async () => {
+    return axiosClient.post('/restablecer', {
+      
+    }).then(function(res){
+        return res.data.results
+    })
+    .catch(function(){
+        throw "Error"
+    })
+}
+
+export const getMailDiferente = async () => {
+    return axiosClient.get('/MailDiferente', {
+       
+    }).then(function(res){
+        return res.data.results
+    })
+    .catch(function(){
+        throw "Error"
+    })
 }
--- screens/IniciarSesion.jsx
+++ screens/IniciarSesion.jsx
@@ -1,116 +1,131 @@
 import React, { Component, useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Date, onChangeDate, number, TextInput, onChangeText, onChangeNumber, String, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, Date, onChangeDate, number, TextInput, onChangeText, onChangeNumber, String, ImageBackground } from 'react-native';
 import BotonIniciarSesion from "../components/BotonIniciarSesion";
 import fondo from "../assets/fondo.jpg";
 import OlvideContraseña from "../components/OlvideContraseña"
+import { getIniciarSesion } from '../Axios/axiosClient';
 
-const IniciarSesion =({navigation})=>{
+const IniciarSesion = ({ navigation }) => {
 
   const [userState, setUserState] = useState({
     email: '',
     password: '',
   });
+
   const [error, setError] = React.useState(false);
   const [disable, setDisable] = React.useState(false);
 
-    return (
-      
-      <View>
+  return (
 
-            <ImageBackground source={fondo} style={styles.image}>
+    <View>
 
-            <Text style={styles.titulo}>Iniciar Sesion</Text>
+      <ImageBackground source={fondo} style={styles.image}>
 
-            <TextInput   
-              style={styles.dato}
-              onChangeText={text => setUserState({ ...userState, email: text })}
-              value={userState.email}
-              placeholder="Correo electronico"
-              
-            />
-            <TextInput
-              style={styles.dato}
-              onChangeText={text => setUserState({ ...userState, password: text })}
-              value={userState.password}
-              placeholder="Contraseña"
-              secureTextEntry={true}
-            />
-               
-               <OlvideContraseña
-                onPress={ () =>{
-                navigation.navigate('RestablecerContraseña')
-                }}
-                 />
+        <Text style={styles.titulo}>Iniciar Sesion</Text>
 
-                 
-            {error && <Text style={styles.alerta}>Completar datos</Text>}
+        <TextInput
+          style={styles.dato}
+          onChangeText={text => setUserState({ ...userState, email: text })}
+          value={userState.email}
+          placeholder="Correo electronico"
+
+        />
+        <TextInput
+          style={styles.dato}
+          onChangeText={text => setUserState({ ...userState, password: text })}
+          value={userState.password}
+          placeholder="Contraseña"
+          secureTextEntry={true}
+        />
+
+        <OlvideContraseña
+          onPress={() => {
+            navigation.navigate('RestablecerContraseña')
+          }}
+        />
+
+
+        {error && <Text style={styles.alerta}>Error</Text>}
 
         <BotonIniciarSesion
-            text="INGRESAR" 
-            onPress={async () =>{
-              setDisable(true)
-              if (userState.email==''||userState.password==''){
-                setError(true)
-              }
-                else {
-                  //await PostLogIn(userState).then(() => {
-                      setDisable(false)
-                      navigation.navigate('SeleccionarComoIngresar')
-                    
-                  //})
-                  //.catch(() => {
-                    //console.log("Datos mal")
-                    //setError(true)
-                   // setDisable(false)
-               // });
+          text="INGRESAR"
+          onPress={async () => {
+            setDisable(true)
+           
+            if (userState.email == '' || userState.password == '') { //si hay datos incompletos
+              setError(true)
               
-          }setDisable(false)
-        }
-      }  
+            }
+            else if (userState.email != '' || userState.password != '') { //si datos completos
+        
+              await getIniciarSesion(userState).then((data) => {
+               
+                if (data == true){ //si la informacion etsa bien
+           
+                setDisable(true)
+
+                navigation.navigate('SeleccionarComoIngresar')
+
+               } else { //si la informacion no etsa bien
+                setError(true) 
+               }
+               if (data != true &&  (userState.email == '' || userState.password == '') ) { //si la informacion no etsa bien y hay datos incompletos
+                setError(true)
+               }
+             
+              })
+                .catch(() => {
+              
+                  setDisable(false)
+                });
+
+            } setDisable(false)
+          }
+          }
         />
-        </ImageBackground>
+      </ImageBackground>
     </View>
-  );  
-  }
-  
-  export default IniciarSesion
-  
-  const styles = StyleSheet.create({
-   
-    titulo: {
-      top: '8%',
-      marginLeft:'-28%',
-      fontSize: 34,
+  );
+}
 
-      },
+export default IniciarSesion
 
-    dato: {
-      fontSize: 18,
-        marginTop:'5%',
-        marginLeft:'0%',
-        width: '80%',
-        alignItems: 'center',
-        borderWidth: 2,
-        padding:'3%',
-        top: '8%',
-        backgroundColor: '#F4F4F4',
+const styles = StyleSheet.create({
 
-      },
-    
-      image: {
-        height:'100%',
-        width: '100%',
-        alignItems: 'center',
-        
-      },
+  titulo: {
+    top: '8%',
+    marginLeft: '-28%',
+    fontSize: 34,
 
-      alerta: {
-        color: 'black',
-        textAlign: 'center',
-        top: '2%',
-        fontSize: 20,
-        width: '100%'
-        },
-   
-        
-  });
+  },
+
+  dato: {
+    fontSize: 18,
+    marginTop: '5%',
+    marginLeft: '0%',
+    width: '80%',
+    alignItems: 'center',
+    borderWidth: 2,
+    padding: '3%',
+    top: '8%',
+    backgroundColor: '#F4F4F4',
+
+  },
+
+  image: {
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+
+  },
+
+  alerta: {
+    color: 'black',
+    textAlign: 'center',
+    top: '2%',
+    fontSize: 20,
+    width: '100%'
+  },
+
+
+});
